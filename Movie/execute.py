d4890b9a75acd7f70554af22d32d60960d62b336
nduckstein <nduckstein@tableau.com>
2019-05-09 10:53:56 -0700

    mysql connection

--- Movie/execute.py
+++ Movie/execute.py
@@ -7,13 +7,14 @@ df_movie_rating, df_movie_tag, df_movie_tag_rating = merge_data(df_movie, df_rat
 df_agg_rating = agg_rating_movie(df_movie_rating)
 df_agg_rating_genre = agg_rating_tag(df_movie_tag_rating)
 
-#print(df_movie[:5], df_rating[:5], df_tag[:5])
-#print(df_movie_tag_rating[:5].drop_duplicates())
-#print(df_agg_rating[:10])
+print(df_movie[:5], df_rating[:5], df_tag[:5])
+print(df_movie_tag_rating[:5].drop_duplicates())
+print(df_agg_rating[:10])
 
 
 #engine = create_engine_pgsql()
 engine = create_engine_mysql()
+
 
 insert_to_db(engine,df_agg_rating,'agg_movie_rating')
 insert_to_db(engine,df_agg_rating_genre,'agg_rating_genre')
